8155e995856bde06eb83ad1135051ac3b85647c3
cameronjoy <camlangdev@gmail.com>
2021-06-16 17:27:39 -0400

    added seed

---
+++ seeders/20210616212254-user.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const { query } = require("express");
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   await query.bulkInsert('users', [{
+     name: 'user'
+   }])
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('users', null, {})
+  }
+};

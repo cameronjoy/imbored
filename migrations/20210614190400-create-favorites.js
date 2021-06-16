ce69beeef52ede32eeade19620857952a97666a9
cameronjoy <camlangdev@gmail.com>
2021-06-16 16:53:10 -0400

    update favorites model

--- migrations/20210614190400-create-favorites.js
+++ migrations/20210614190400-create-favorites.js
@@ -14,6 +14,9 @@ module.exports = {
       activityKey: {
         type: Sequelize.STRING
       },
+      note: {
+        type: Sequelize.STRING
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
